87898b55a4aeaf30100dbb4246ca637a7a06a626
Amadou Diallo <amadou.diallobtsio@gmail.com>
2023-11-05 17:47:41 +0100

    Implementation de aout d'une tache

--- Projet_1/GestionnaireDeTache.c
+++ Projet_1/GestionnaireDeTache.c
@@ -1,17 +1,53 @@
 #include <stdio.h>
+#include <string.h>
 
+
+ const int MAX_TACHES = 100;
 int main() {
-    char affichages[4][100] = {
-        "1: Ajouter une tache",
-        "2: supprimer une tache",
-        "3: Mettre a jour une tache",
-        "4: Afficher les tache",
-    };
+   
+    char taches[MAX_TACHES][100]; 
+    int compteurDeTaches = 0;
 
-     for (int i = 0; i < 4; i++) {
-        printf("%s \n", affichages[i]);
-        printf("------------------------\n");
-    }
+    int choix;
+    do {
+        printf("Menu :\n");
+        printf("1: Ajouter une tâche\n");
+        printf("2: Supprimer une tâche\n");
+        printf("3: Mettre à jour une tâche\n");
+        printf("4: Afficher les tâches\n");
+        printf("0: Quitter\n");
+        printf("Choisissez une option : ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1:
+                if (compteurDeTaches < MAX_TACHES) {
+                    printf("Entrez la description de la tâche : ");
+                    scanf(" %[^\n]", taches[compteurDeTaches]);
+                    compteurDeTaches++;
+                } else {
+                    printf("Le tableau de tâches est plein. Impossible d'ajouter une nouvelle tâche.\n");
+                }
+                break;
+            case 2:
+                
+                break;
+            case 3:
+
+                break;
+            case 4:
+                printf("Tâches :\n");
+                for (int i = 0; i < compteurDeTaches; i++) {
+                    printf("%d: %s\n", i + 1, taches[i]);
+                }
+                break;
+            case 0:
+                printf("Au revoir !\n");
+                break;
+            default:
+                printf("Option non valide. Veuillez choisir une option valide.\n");
+        }
+    } while (choix != 0);
 
     return 0;
 }
