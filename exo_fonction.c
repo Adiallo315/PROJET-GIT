10aca38912c7b8f8858be29b14428bd7ed342609
Germain Dimi <mauricedimi2002@gmail.com>
2023-11-12 22:33:41 +0100

    ajout d'un exo fonction

---
+++ exo_fonction.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+#Exercice addition
+int main(){
+    int a = 3;
+    int b = 5;
+    int resultat=0;
+
+    resultat = a + b ;
+
+    printf("%d + %d = %d", a, b, resultat);
+
+}
