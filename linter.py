2a2308ce37a9ace913c14ada523b3cd9216f22c5
Amadou Diallo <amadou.diallobtsio@gmail.com>
2023-11-13 11:13:38 +0100

    Ajout du linter en c

---
+++ linter.py
@@ -0,0 +1,23 @@
+import re
+
+def lint_javascript(code):
+    lines = code.split('\n')
+
+    errors = []
+    for i, line in enumerate(lines, start=1):
+        
+        if not re.search(r';\s*$', line):
+            errors.append(f"Ligne {i}: Pas de point-virgule à la fin de la ligne")
+
+    return errors
+
+with open("test_script.js", "r") as file:
+    javascript_code = file.read()
+
+linting_errors = lint_javascript(javascript_code)
+
+if linting_errors:
+    for error in linting_errors:
+        print(f"Erreur de style : {error}")
+else:
+    print("Le code respecte les règles de style.")
---
+++ test_script.js
@@ -0,0 +1,4 @@
+let message = "Bonjour, monde!";
+let message2 = "Bonjour,les humains!"
+let message3 = "Bonjour,";
+let message4 = "Bonjour,les humains!"
